1028cad7c5c797cc0f525f5c18a656c90f4cb2c1
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-08-02 23:07:21 -0300

    Add files via upload

---
+++ entrada_saida.rb
@@ -0,0 +1,9 @@
+#saída de dados
+print 'Digite seu nome: '
+
+#Recebendo uma entrada do teclado
+name = gets.chomp
+
+#saída utilizando puts
+#use códigos ruby dentro de uma string #{code}
+puts "Hello #{name}!"
