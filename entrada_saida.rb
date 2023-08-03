6e0e764a8daa903666237d1b7a052003604dffe9
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2023-08-02 23:20:50 -0300

    Add files via upload

--- entrada_saida.rb
+++ entrada_saida.rb
@@ -4,6 +4,10 @@ print 'Digite seu nome: '
 #Recebendo uma entrada do teclado
 name = gets.chomp
 
+print 'Digite seu sobrenome: '
+
+sobrenome = gets.chomp
+
 #saída utilizando puts
 #use códigos ruby dentro de uma string #{code}
-puts "Hello #{name}!"
+puts "Hello #{name} #{sobrenome}!"
